a6eab295755105b77d9c6269da228e73b6c7dab6
wafflejuice <wafflejuice96@gmail.com>
2023-05-09 21:52:05 +0900

    detail post

---
+++ coding-apple-nextjs-forum/app/detail/[id]/page.js
@@ -0,0 +1,17 @@
+import { connectDB } from '@/util/database';
+import { ObjectId } from 'mongodb';
+
+export default async function Detail(props) {
+  console.log(props)
+  const db = (await connectDB).db('forum')
+  let result = await db.collection('post').findOne({ _id: new ObjectId(props.params.id) })
+  console.log(result);
+
+  return (
+    <div>
+      <h4>detail page</h4>
+      <h4>{result.title}</h4>
+      <p>{result.content}</p>
+    </div>
+  );
+}
--- coding-apple-nextjs-forum/app/list/page.js
+++ coding-apple-nextjs-forum/app/list/page.js
@@ -1,19 +1,25 @@
 import { connectDB } from "@/util/database"
+import Link from "next/link";
+import DetailLink from "./DetailLink";
 
 export default async function List() {
   const db = (await connectDB).db('forum')
   let result = await db.collection('post').find().toArray()
-  console.log(result)
 
   return (
     < div className="list-bg" >
       {
         result.map((e, i) => {
           return (
-            <div className="list-item" key={i}>
-              <h4>{e.title}</h4>
-              <p>{e.content}</p>
-            </div>
+            <>
+              <div className="list-item" key={i}>
+                <Link href={`/detail/${e._id}`}>
+                  <h4>{e.title}</h4>
+                </Link>
+                <p>{e.content}</p>
+                <DetailLink />
+              </div>
+            </>
           );
         })
       }
