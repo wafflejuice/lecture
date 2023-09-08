013da4e3f05f0a3cf8dc490cf4e921a7c35616e0
wafflejuice <wafflejuice96@gmail.com>
2023-09-08 23:13:02 +0900

    spring-db-1 initial commit

---
+++ spring-db-1/src/main/kotlin/hello/jdbc/SpringDb1Application.kt
@@ -0,0 +1,11 @@
+package hello.jdbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDb1Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringDb1Application>(*args)
+}
---
+++ spring-db-1/src/test/kotlin/hello/jdbc/SpringDb1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package hello.jdbc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDb1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
