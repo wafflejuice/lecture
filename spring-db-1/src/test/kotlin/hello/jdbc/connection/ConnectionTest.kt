224c041acd844c9221593a28a3d4cccbd1920b64
wafflejuice <wafflejuice96@gmail.com>
2023-09-15 23:18:43 +0900

    커넥션풀과 데이터소스 이해 > DataSource 예제2 - 커넥션 풀

--- spring-db-1/src/test/kotlin/hello/jdbc/connection/ConnectionTest.kt
+++ spring-db-1/src/test/kotlin/hello/jdbc/connection/ConnectionTest.kt
@@ -1,5 +1,6 @@
 package hello.jdbc.connection
 
+import com.zaxxer.hikari.HikariDataSource
 import hello.jdbc.connection.ConnectionConst.PASSWORD
 import hello.jdbc.connection.ConnectionConst.URL
 import hello.jdbc.connection.ConnectionConst.USERNAME
@@ -29,11 +30,34 @@ internal class ConnectionTest {
         useDataSource(dataSource = dataSource)
     }
 
+    @Test
+    fun dataSourceConnectionPool() {
+        // Connection Pooling
+        val dataSource = HikariDataSource()
+        dataSource.jdbcUrl = URL
+        dataSource.username = USERNAME
+        dataSource.password = PASSWORD
+        dataSource.maximumPoolSize = 10
+        dataSource.poolName = "MyPool"
+
+        useDataSource(dataSource)
+        Thread.sleep(3000)
+    }
+
     private fun useDataSource(dataSource: DataSource) {
         val con1 = dataSource.connection
         val con2 = dataSource.connection
 
         logger.info("connection=$con1, class=${con1.javaClass}")
         logger.info("connection=$con2, class=${con2.javaClass}")
+
+        /* stuck example
+        val connections = mutableListOf<Connection>()
+        (1..11).forEach { _ ->
+            val con = dataSource.connection
+            connections.add(con)
+            logger.info("connection=$con, class=${con.javaClass}")
+        }
+         */
     }
 }
