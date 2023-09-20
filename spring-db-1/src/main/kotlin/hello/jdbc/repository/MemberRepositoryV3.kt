ed1ce8f64cc7e3c36c808210590ce21694dc321d
wafflejuice <wafflejuice96@gmail.com>
2023-09-20 23:58:43 +0900

    스프링과 문제 해결 - 트랜잭션 > 트랜잭션 문제 해결 - 트랜잭션 매니저1 #1

---
+++ spring-db-1/src/main/kotlin/hello/jdbc/repository/MemberRepositoryV3.kt
@@ -0,0 +1,132 @@
+package hello.jdbc.repository
+
+import hello.jdbc.domain.Member
+import org.slf4j.LoggerFactory
+import org.springframework.jdbc.datasource.DataSourceUtils
+import org.springframework.jdbc.support.JdbcUtils
+import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.sql.Statement
+import javax.sql.DataSource
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ * DataSourceUtils.getConnection()
+ * DataSourceUtils.releaseConnection()
+ */
+class MemberRepositoryV3(
+    private val dataSource: DataSource,
+) {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    fun save(member: Member): Member {
+        val sql = "insert into member(member_id, money) values (?, ?)"
+
+        var con: Connection? = null
+        var pstmt: PreparedStatement? = null
+
+        try {
+            con = getConnection()
+            pstmt = con.prepareStatement(sql)
+            pstmt.setString(1, member.memberId)
+            pstmt.setInt(2, member.money)
+            pstmt.executeUpdate()
+
+            return member
+        } catch (e: SQLException) {
+            logger.error("db error", e)
+            throw e
+        } finally {
+            close(con = con, stmt = pstmt, rs = null)
+        }
+    }
+
+    fun findById(memberId: String): Member {
+        val sql = "select * from member where member_id = ?"
+
+        var con: Connection? = null
+        var pstmt: PreparedStatement? = null
+        var rs: ResultSet? = null
+
+        try {
+            con = getConnection()
+            pstmt = con.prepareStatement(sql)
+            pstmt.setString(1, memberId)
+            rs = pstmt.executeQuery()
+            if (rs.next()) {
+                return Member(
+                    memberId = rs.getString("member_id"),
+                    money = rs.getInt("money"),
+                )
+            } else {
+                throw NoSuchElementException("member not found memberId=$memberId")
+            }
+        } catch (e: SQLException) {
+            logger.error("db error", e)
+            throw e
+        } finally {
+            close(con = con, stmt = pstmt, rs = rs)
+        }
+    }
+
+    fun update(memberId: String, money: Int) {
+        val sql = "update member set money=? where member_id=?"
+
+        var con: Connection? = null
+        var pstmt: PreparedStatement? = null
+
+        try {
+            con = getConnection()
+            pstmt = con.prepareStatement(sql)
+            pstmt.setInt(1, money)
+            pstmt.setString(2, memberId)
+
+            val resultSize = pstmt.executeUpdate()
+            logger.info("resultSize=$resultSize")
+        } catch (e: SQLException) {
+            logger.error("db error", e)
+            throw e
+        } finally {
+            close(con = con, stmt = pstmt, rs = null)
+        }
+    }
+
+    fun delete(memberId: String) {
+        val sql = "delete from member where member_id=?"
+
+        var con: Connection? = null
+        var pstmt: PreparedStatement? = null
+
+        try {
+            con = getConnection()
+            pstmt = con.prepareStatement(sql)
+            pstmt.setString(1, memberId)
+
+            pstmt.executeUpdate()
+        } catch (e: SQLException) {
+            logger.error("db error", e)
+            throw e
+        } finally {
+            close(con = con, stmt = pstmt, rs = null)
+        }
+    }
+
+    private fun close(con: Connection?, stmt: Statement?, rs: ResultSet?) {
+        JdbcUtils.closeResultSet(rs)
+        JdbcUtils.closeStatement(stmt)
+
+        // 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
+        DataSourceUtils.releaseConnection(con, dataSource)
+    }
+
+    private fun getConnection(): Connection {
+        // 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
+        val con = DataSourceUtils.getConnection(dataSource)
+
+        logger.info("get connection=${con}, class=${con::class}")
+        return con
+    }
+}
