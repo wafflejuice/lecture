dc744e6f1d2d7a9745926c30b9e8254654b0c567
wafflejuice <wafflejuice96@gmail.com>
2023-09-25 23:54:30 +0900

    inflearn-querydsl initial commit

---
+++ inflearn-querydsl/src/main/kotlin/study/querydsl/InflearnQuerydslApplication.kt
@@ -0,0 +1,11 @@
+package study.querydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InflearnQuerydslApplication
+
+fun main(args: Array<String>) {
+    runApplication<InflearnQuerydslApplication>(*args)
+}
