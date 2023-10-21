ae248b945bc86741180447ef6693ef979dd5f7fb
wafflejuice <wafflejuice96@gmail.com>
2023-10-21 21:44:01 +0900

    스프링 데이터 JPA가 제공하는 Querydsl 기능 > 인터페이스 지원 - QuerydslPredicateExecutor

--- inflearn-querydsl/src/test/kotlin/study/querydsl/repository/MemberRepositoryTest.kt
+++ inflearn-querydsl/src/test/kotlin/study/querydsl/repository/MemberRepositoryTest.kt
@@ -11,6 +11,7 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.transaction.annotation.Transactional
 import study.querydsl.dto.MemberSearchCondition
 import study.querydsl.entity.Member
+import study.querydsl.entity.QMember.member
 import study.querydsl.entity.Team
 
 @SpringBootTest
@@ -111,5 +112,31 @@ internal class MemberRepositoryTest {
 
         assertEquals(3, result.size)
         assertIterableEquals(listOf("member1", "member2", "member3"), result.content.map { it.username })
+    }
+
+    @Test
+    fun querydslPredicateExecutorTest() {
+        val teamA = Team.new("teamA")
+        val teamB = Team.new("teamB")
+
+        em.persist(teamA)
+        em.persist(teamB)
+
+        val member1 = Member.new("member1", 10, teamA)
+        val member2 = Member.new("member2", 20, teamA)
+
+        val member3 = Member.new("member3", 30, teamB)
+        val member4 = Member.new("member4", 40, teamB)
+
+        em.persist(member1)
+        em.persist(member2)
+        em.persist(member3)
+        em.persist(member4)
+
+        val result = memberRepository.findAll(
+            member.age.between(10, 40)
+                .and(member.username.eq("member1"))
+        )
+        result.forEach { member -> println("member = $member") }
     }
 }
